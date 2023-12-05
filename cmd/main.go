75d9e9f1605dc0322bff988e98944d9832324749
Eduardo Pousa <pousaedu@protonmail.com>
2023-12-05 11:38:42 +0000

    function getRequest

--- cmd/main.go
+++ cmd/main.go
@@ -10,40 +10,37 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var url string = "https://api.nasa.gov/planetary/apod?api_key="
-var method string = "GET"
-
 func init() {
 	if err := godotenv.Load("../.env"); err != nil {
 		log.Print("No .env file found")
 	}
 }
 
-func main() {
-	authKey := os.Getenv("APOD_KEY")
-	if authKey == "" {
-		log.Fatal("AUTH_TOKEN not set")
-	}
+func getRequest(client *http.Client, url string) string {
 
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url+authKey, nil)
-
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Println(string(body))
+
+	return string(body)
+}
+
+func main() {
+	client := &http.Client{}
+
+	apiData := getRequest(client, os.Getenv("APOD")+os.Getenv("NASA_KEY"))
+
+	fmt.Println(apiData)
 }
